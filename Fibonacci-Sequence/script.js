ecf3a206108c66547f807edffdf4384819d903db
Pesar <pesar369@gmail.com>
2022-09-01 00:42:13 +0430

    fixing branchs

---
+++ Fibonacci-Sequence/script.js
@@ -0,0 +1,13 @@
+function FibonacciNumber(length) {
+  let a = 0,
+    b = 1,
+    sum = 0;
+  for (let i = 2; i <= length; i++) {
+    sum = a + b;
+    a = b;
+    b = sum;
+  }
+  return sum;
+}
+
+console.log(FibonacciNumber(11));
