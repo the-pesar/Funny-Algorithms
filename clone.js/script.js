eeb25481a361824259fd240f86bb67dab80a709e
Navid079 <navid.lee79@gmail.com>
2022-09-03 16:32:36 +0430

    clean code and cb-hell fixed

--- clone.js/script.js
+++ clone.js/script.js
@@ -3,11 +3,34 @@ const fs = require("fs");
 
 const n = 10;
 
+const [command] = process.argv.slice(2);
+if (command === "clean") {
+  for (let i = n - 1; i >= 0; i--) {
+    if (fs.existsSync(`${i}.js`)) {
+      fs.unlinkSync(`${i}.js`);
+    }
+  }
+  process.exit(0);
+}
+
+const runClone = err => {
+  if (err) {
+    console.log(`Cloning to ${n - 1}.js failed. Aborting...`);
+  }
+
+  cp.execSync(`node ${n - 1}.js`);
+};
+
+const cloneFile = (err, data) => {
+  if (err) {
+    console.log(`Opening file ${__filename} failed. Aborting...`);
+    return;
+  }
+
+  const clone = data.replace(`${n}`, `${n - 1}`);
+  fs.writeFile(`${n - 1}.js`, clone, runClone);
+};
+
 if (n > 0) {
-  fs.readFile(__filename, { encoding: "utf-8" }, (err, data) => {
-    const clone = data.replace(`${n}`, `${n - 1}`);
-    fs.writeFile(`${n - 1}.js`, clone, err => {
-      cp.execSync(`node ${n - 1}.js`,);
-    });
-  });
+  fs.readFile(__filename, { encoding: "utf-8" }, cloneFile);
 }
