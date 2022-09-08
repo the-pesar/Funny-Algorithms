2442ba5cc309f13fa498319e8ede649dc1116704
mahsa <mahsaawb.r@gmail.com>
2022-09-08 20:53:59 +0430

    Updated

--- DP-1/DP-Code.cpp
+++ DP-1/DP-Code.cpp
@@ -1,24 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-// author : Mahsa_rz
-const int MAXN = 1E5 + 3;
-
 typedef long long ll;
-
-ll n , arr[MAXN] , m;
-
+const int maxn = 2e3+10;
+int dp[maxn][maxn], a[maxn], b[maxn], n, m, sub[maxn];
 int main(){
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    cin >> m;
+    for (int i = 1; i <= m; i++)
+        cin >> b[i];
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            if (a[i] == b[j]){
+                dp[i][j] = max(dp[i - 1][j - 1] + 1, dp[i][j - 1]);
+            } else{
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
 
-	ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+    cout << dp[n][m] << endl;
+    /***************************************************/
 
-	cin >> n;
-	for (int i = 0 ; i < n ; i++) {
-		cin >> m;
-		arr[m] += m; // calculate sum of numbers => points
-	}
-	for (int i = 2 ; i < MAXN ; i++) 
-		arr[i] = max(arr[i - 1] , arr[i] + arr[i - 2]); // dp => check for max point
+    int i = n, j = m, inx = dp[n][m];
+    while (i > 0 and j > 0) {
+        if (a[i] == b[j]) {
+            sub[inx] = a[i];
+            i--;
+            j--;
+            inx--;
+            continue;
+        }
+        dp[i - 1][j] > dp[i][j - 1] ? (i--) : (j--);
+    }
 
-	cout << arr[MAXN - 1] << endl;
+    for (int i = inx + 1; i <= dp[n][m] ; i++)
+        cout << sub[i] << ' ';
 }
